05336e2f83186dfab07491c96897d9a263b976f1
Julius de Boer <JuliusDeBoer@users.noreply.github.com>
2023-05-16 13:25:11 +0200

    Mouse movement is really neat!

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -6,5 +6,12 @@ public partial class MainPage : ContentPage
 	{
 		InitializeComponent();
 	}
-}
 
+    void OnPointerMoved(object sender, PointerEventArgs e)
+    {
+		Point? pos = e.GetPosition(this);
+		if(!pos.HasValue) { return; }
+
+		Title = $"X: {pos.Value.X} Y: {pos.Value.Y}";
+    }
+}
