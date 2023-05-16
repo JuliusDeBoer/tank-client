9b8d72b73958aae6463e19d5971cfb355d3100f9
Julius de Boer <JuliusDeBoer@users.noreply.github.com>
2023-05-16 15:07:16 +0200

    You can now actually move the thingy. Yay!

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -2,16 +2,46 @@
 
 public partial class MainPage : ContentPage
 {
-	public MainPage()
-	{
-		InitializeComponent();
-	}
+    private bool pressed = false;
+    private bool shouldUpdateAnchor = true;
+    private double anchorX = 0;
+    private double anchorY = 0;
 
-    void OnPointerMoved(object sender, PointerEventArgs e)
+    public MainPage()
     {
-		Point? pos = e.GetPosition(this);
-		if(!pos.HasValue) { return; }
+        InitializeComponent();
+    }
 
-		Title = $"X: {pos.Value.X} Y: {pos.Value.Y}";
+    void MoveBattleground(object sender, PointerEventArgs e)
+    {
+        if (!pressed) { return; }
+
+
+        Point? pos = e.GetPosition(this);
+        if (!pos.HasValue) { return; }
+
+
+        if (shouldUpdateAnchor)
+        {
+            anchorX = pos.Value.X - battleground.TranslationX;
+            anchorY = pos.Value.Y - battleground.TranslationY;
+            shouldUpdateAnchor = false;
+        }
+
+        Title = $"X: {pos.Value.X} Y: {pos.Value.Y}";
+
+        battleground.TranslationX = pos.Value.X - anchorX;
+        battleground.TranslationY = pos.Value.Y - anchorY;
+    }
+
+    private void BattlegroundPressed(object sender, EventArgs e)
+    {
+        shouldUpdateAnchor = true;
+        pressed = true;
+    }
+
+    private void BattlegroundReleased(object sender, EventArgs e)
+    {
+        pressed = false;
     }
 }
