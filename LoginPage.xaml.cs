5c8ff862362323efbd1a69719f064cbceb771d34
Julius de Boer <JuliusDeBoer@users.noreply.github.com>
2023-05-30 14:46:05 +0200

    Add logging in

--- LoginPage.xaml.cs
+++ LoginPage.xaml.cs
@@ -9,11 +9,34 @@ public partial class LoginPage : ContentPage
 		InitializeComponent();
 	}
 
-    private void SignUp(Object sender, EventArgs e)
+// This is dumb
+#nullable enable
+
+	private void SignUp(Object sender, EventArgs e)
     {
-		Server.Invoke("CreateAccount", NameEntry.Text, EmailEntry.Text, PasswordEntry.Text);
-		string jwt = Server.Invoke<string>("Login", EmailEntry.Text, PasswordEntry.Text);
+		Server.Invoke("CreateAccount", SignUpNameEntry.Text, SignUpEmailEntry.Text, SignUpPasswordEntry.Text);
+		string? jwt = Server.Invoke<string>("Login", SignUpEmailEntry.Text, SignUpPasswordEntry.Text);
+
+		if (jwt == null)
+		{
+			ErrorLabel.Text = "Error signing up";
+			return;
+		}
 
         Navigation.PushAsync(new MainPage());
     }
+    private void Login(Object sender, EventArgs e)
+    {
+		string? jwt = Server.Invoke<string>("Login", LoginEmailEntry.Text, LoginPasswordEntry.Text);
+
+		if (jwt == null)
+		{
+			ErrorLabel.Text = "Error loggin in";
+			return;
+		}
+
+		Navigation.PushAsync(new MainPage());
+    }
+
+#nullable disable
 }
