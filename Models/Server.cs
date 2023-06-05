3033f1626d87b34e440cca964a035f7a04c165d7
Julius de Boer <JuliusDeBoer@users.noreply.github.com>
2023-06-05 11:30:30 +0200

    "If we confess our sins, he is faithful and just and will forgive us our sins and purify us from all unrighteousness."
    - 1 john 1:9

--- Models/Server.cs
+++ Models/Server.cs
@@ -23,7 +23,54 @@ namespace tank_client.Models
             }
         }
 
-        // Im gonna cry
+        //
+        // 1 john 1:9
+        //
+
+        public static void On(string methodName, Action handler)
+        {
+            Connection.On(methodName, handler);
+        }
+
+        public static void On<T1>(string methodName, Action<T1> handler)
+        {
+            Connection.On<T1>(methodName, handler<T1>);
+        }
+
+        public static void On<T1, T2>(string methodName, Action<T1, T2> handler)
+        {
+            Connection.On<T1, T2>(methodName, handler<T1, T2>);
+        }
+
+        public static void On<T1, T2, T3>(string methodName, Action<T1, T2, T3> handler)
+        {
+            Connection.On<T1, T2, T3>(methodName, handler<T1, T2, T3>);
+        }
+
+        public static void On<T1, T2, T3, T4>(string methodName, Action<T1, T2, T3, T4> handler)
+        {
+            Connection.On<T1, T2, T3, T4>(methodName, handler<T1, T2, T3, T4>);
+        }
+
+        public static void On<T1, T2, T3, T4, T5>(string methodName, Action<T1, T2, T3, T4, T5> handler)
+        {
+            Connection.On<T1, T2, T3, T4, T5>(methodName, handler<T1, T2, T3, T4, T5>);
+        }
+
+        public static void On<T1, T2, T3, T4, T5, T6>(string methodName, Action<T1, T2, T3, T4, T5, T6> handler)
+        {
+            Connection.On<T1, T2, T3, T4, T5, T6>(methodName, handler<T1, T2, T3, T4, T5, T6>);
+        }
+
+        public static void On<T1, T2, T3, T4, T5, T6, T7>(string methodName, Action<T1, T2, T3, T4, T5, T6, T7> handler)
+        {
+            Connection.On<T1, T2, T3, T4, T5, T6, T7>(methodName, handler<T1, T2, T3, T4, T5, T6, T7>);
+        }
+        public static void On<T1, T2, T3, T4, T5, T6, T7, T8>(string methodName, Action<T1, T2, T3, T4, T5, T6, T7, T8> handler)
+        {
+            Connection.On<T1, T2, T3, T4, T5, T6, T7, 8>(methodName, handler<T1, T2, T3, T4, T5, T6, T7, T8>);
+        }
+
         public static T Invoke<T>(string methodName)
         {
             Task<T> task = Connection.InvokeAsync<T>(methodName);
@@ -84,6 +131,61 @@ namespace tank_client.Models
         public static void Invoke(string methodName, object arg1, object arg2, object arg3, object arg4, object arg5)
         {
             Connection.InvokeAsync(methodName, arg1, arg2, arg3, arg4, arg5);
+        }
+
+        public static T Invoke<T>(string methodName, object arg1, object arg2, object arg3, object arg4, object arg5, object arg6)
+        {
+            Task<T> task = Connection.InvokeAsync<T>(methodName, arg1, arg2, arg3, arg4, arg5, arg6);
+            return task.Result;
+        }
+
+        public static void Invoke(string methodName, object arg1, object arg2, object arg3, object arg4, object arg5, object arg6)
+        {
+            Connection.InvokeAsync(methodName, arg1, arg2, arg3, arg4, arg5, arg6);
+        }
+
+        public static void Invoke(string methodName, object arg1, object arg2, object arg3, object arg4, object arg5, object arg6, object arg7)
+        {
+            Connection.InvokeAsync(methodName, arg1, arg2, arg3, arg4, arg5, arg6, arg7);
+        }
+
+        public static T Invoke<T>(string methodName, object arg1, object arg2, object arg3, object arg4, object arg5, object arg6, object arg7)
+        {
+            Task<T> task = Connection.InvokeAsync<T>(methodName, arg1, arg2, arg3, arg4, arg5, arg6, arg7);
+            return task.Result;
+        }
+
+        public static void Invoke(string methodName, object arg1, object arg2, object arg3, object arg4, object arg5, object arg6, object arg7, object arg8)
+        {
+            Connection.InvokeAsync(methodName, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8);
+        }
+
+        public static T Invoke<T>(string methodName, object arg1, object arg2, object arg3, object arg4, object arg5, object arg6, object arg7, object arg8)
+        {
+            Task<T> task = Connection.InvokeAsync<T>(methodName, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8);
+            return task.Result;
+        }
+
+        public static void Invoke(string methodName, object arg1, object arg2, object arg3, object arg4, object arg5, object arg6, object arg7, object arg8, object arg9)
+        {
+            Connection.InvokeAsync(methodName, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9 );
+        }
+
+        public static T Invoke<T>(string methodName, object arg1, object arg2, object arg3, object arg4, object arg5, object arg6, object arg7, object arg8, object arg9)
+        {
+            Task<T> task = Connection.InvokeAsync<T>(methodName, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9);
+            return task.Result;
+        }
+
+        public static void Invoke(string methodName, object arg1, object arg2, object arg3, object arg4, object arg5, object arg6, object arg7, object arg8, object arg9, object arg10)
+        {
+            Connection.InvokeAsync(methodName, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, arg10);
+        }
+
+        public static T Invoke<T>(string methodName, object arg1, object arg2, object arg3, object arg4, object arg5, object arg6, object arg7, object arg8, object arg9, object arg10)
+        {
+            Task<T> task = Connection.InvokeAsync<T>(methodName, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, arg10);
+            return task.Result;
         }
     }
 }
